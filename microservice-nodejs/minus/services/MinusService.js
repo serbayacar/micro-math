7bd21cce2a8370e77fb9f9cdf77931426b960469
Serbay ACAR <serbayacar@gmail.com>
2022-07-31 10:24:33 +0300

    Standarize :: all result json data getting same

--- microservice-nodejs/minus/services/MinusService.js
+++ microservice-nodejs/minus/services/MinusService.js
@@ -2,9 +2,9 @@
 
 const minus = (req, res, next) => {
     try {
-        let number1 = Number.parseFloat(req.body.number1);
-        let number2 = Number.parseFloat(req.body.number2)
-        res.status(200).json({ "minus": number1 - number2 });
+        let number1 = Number.parseFloat(req.query.number1);
+        let number2 = Number.parseFloat(req.query.number2)
+        res.status(200).json({ "result": number1 - number2 });
     }catch(error) {
         next(error)
     }
